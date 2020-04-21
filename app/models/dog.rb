e612745c5ed82225fdd4188bdd45bb129b371d45
lightfire224 <elias.taveras@columbia.edu>
2020-04-21 11:29:15 -0400

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    @@all = []
+    attr_accessor :name, :breed, :age
+    def initialize(name, breed,age)
+        @name = name
+        @breed = breed
+        @age = age
+        @@all << self
+    end
+
+    def Dog.all
+        @@all
+    end
+end
